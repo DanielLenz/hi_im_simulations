Fix spectral axis units.

src/features/box_params.py
@@ -16,22 +16,30 @@ def create_header(box):
 
     header['CDELT1'] = box['pixsize'].value
     header['CDELT2'] = box['pixsize'].value
-    header['CDELT3'] = box['delta_v'].value
+    header['CDELT3'] = -box['delta_nu'].value
 
     header['CRPIX1'] = 0
     header['CRPIX2'] = 0
     header['CRPIX3'] = 0
 
+    header['CUNIT1'] = 'deg'
+    header['CUNIT2'] = 'deg'
+    header['CUNIT3'] = 'Hz'
+
     header['CRVAL1'] = box['ra_min'].value
     header['CRVAL2'] = box['dec_min'].value
-    header['CRVAL3'] = box['velocity_grid'][0].value
+    header['CRVAL3'] = box['nus'][0].value
     header['LATPOLE'] = 90.
 
     header['CTYPE1'] = 'RA---SFL'
     header['CTYPE2'] = 'DEC--SFL'
-    header['CTYPE3'] = 'VRAD'
+    header['CTYPE3'] = 'FREQ'
 
     return header
+
+
+def parkes_type():
+    ...
 
 
 def get_test_params():
@@ -49,18 +57,17 @@ def get_test_params():
 
     # spectral
     z_start = 0.01  # redshift start
-    bandwidth = 1. * u.MHz  # bandwidth in MHz
+    bandwidth = (100 * u.MHz).to(u.Hz)  # bandwidth in MHz
     nu_start = P.HI_RESTFREQ / (1. + z_start)
     n_channels = 512  # 512 spectral channels
     # delta_nu = 10. * u.kHz  # spacing in frequency
     delta_nu = bandwidth / n_channels  # spacing in frequency
 
-    # get frequencies
+    # get frequencies, high to low freqs, meaning increasing redshift
     nus = np.linspace(
         nu_start.to(u.Hz).value,
-        (nu_start + n_channels * delta_nu).to(u.Hz).value,
+        (nu_start - n_channels * delta_nu).to(u.Hz).value,
         n_channels) * u.Hz
-    nus = nus[::-1]  # high to low freqs, meaning increasing redshift
 
     # convert to v_lsr in km/s
     velocity_grid = nus.to(u.km / u.s, equivalencies=P.HI_VFRAME)

src/features/gal_params.py
@@ -30,8 +30,8 @@ def get_simple_priors(v_range, n_samples=1):
     # rotation velocities
     inclination = np.random.uniform(0, 360., n_samples)
     rot_velos = stats.norm.rvs(loc=200, scale=50, size=n_samples)
-    rot_velos = np.clip(rot_velos, a_min=50., a_max=None)
-    eff_rot_velos = rot_velos * np.sin(np.radians(inclination))
+    eff_rot_velos = np.abs(rot_velos * np.sin(np.radians(inclination)))
+    eff_rot_velos = np.clip(eff_rot_velos, a_min=50., a_max=None)
 
     # skewness, has to be within [-1, 1]
     skewness = stats.norm.rvs(loc=0, scale=0.3, size=n_samples)

src/features/gridding.py
@@ -39,11 +39,14 @@ def grid_hpx(healpix_data, header, beamsize):
     npix = hp.nside2npix(nside)
 
     if healpix_data.ndim == 1:
-        header['NAXIS3'] = 1
-        healpix_data = healpix_data[:, None]
-
+        h = header.copy()
+        h['NAXIS3'] = 1
+        d = healpix_data[:, None]
+    else:
+        h = header.copy()
+        d = healpix_data
     # set up the gridder and kernel
-    gridder = cygrid.WcsGrid(header)
+    gridder = cygrid.WcsGrid(h)
 
     # set kernel
     kernelsize_fwhm = 3. * beamsize
@@ -62,7 +65,7 @@ def grid_hpx(healpix_data, header, beamsize):
     lats = (90. - np.rad2deg(theta)).astype(np.float64)
 
     # grid
-    gridder.grid(lons, lats, healpix_data)
+    gridder.grid(lons, lats, d)
     datacube = gridder.get_datacube()
 
     return np.squeeze(datacube)

src/features/spectra.py
@@ -28,7 +28,12 @@ def generate_spectra(gal_params, ra, dec, velocity_grid, v_rad):
             skewness=gal_params['skewness'][i])
         specs.append(spectrum.spectrum)
 
-    return np.array(specs)
+    specs = np.array(specs)
+
+    print(velocity_grid)
+    print(v_rad)
+    print(np.argmax(specs, 1))
+    return specs
 
 
 class Spectrum(object):
@@ -43,6 +48,7 @@ class Spectrum(object):
             velo_center, flux, v_rot, turb_velo, solid_rot, skewness):
 
         self.velos = velos
+        self.velo_center = velo_center
         self.flux = flux
         self.logflux = np.log10(self.flux)
         self.v_rot = v_rot
@@ -68,6 +74,7 @@ class Spectrum(object):
         if self._shape_parameters is None:
             self._shape_parameters = np.array([
                 self.logflux,
+                self.velo_center,
                 self.v_rot,
                 self.turb_velo,
                 self.solid_rot,

src/simulations/generate_noise.py
@@ -10,7 +10,7 @@ def generate_noise(box, gal_priors, locations):
 
     shape = box['n_channels'], box['boxsize'], box['boxsize']
 
-    noise_cube = np.random.normal(loc=0, scale=1., size=shape)
+    noise_cube = np.random.normal(loc=0, scale=2., size=shape)
 
     return noise_cube
 